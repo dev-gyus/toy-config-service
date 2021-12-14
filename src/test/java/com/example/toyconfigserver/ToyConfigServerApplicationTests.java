469b648078efa09a9535bc6ed3c3e2f79121adb4
GyuHyeong <cjsworbehd13@gmail.com>
2021-12-14 23:00:44 +0900

    first commit

---
+++ src/test/java/com/example/toyconfigserver/ToyConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toyconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
